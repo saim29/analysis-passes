11784201e9419d97d3424398ed836ce38c9de9db
Tanmaya Mishra <tanmayam@vt.edu>
2021-04-02 16:47:56 -0400

    Moved stuff around

--- dead-code-elimination.cpp
+++ dead-code-elimination.cpp
@@ -13,130 +13,124 @@ namespace {
     return BitVector();
   }
 
-  class faintAnalysis {
-
-    public:
-      faintAnalysis(Function &func) {
-
-        map_indexes(func);
-        populate_lhs(func);
-        populate_rhs(func);
-        populate_use(func);
-        
-      }
-
-      void map_indexes(Function &F) {
-
-        unsigned ind = 0;
-
-        for (BasicBlock &B : F) {
-
-          for (Instruction &I : B) {
-
-            if (I.isUsedInBasicBlock(&B) || I.isUsedOutsideOfBlock(&B)) {
-
-              bvec_mapping.insert({&I, ind});
-              ind++;
-
-            }
-          }
-        }
-      }
-
-      void populate_lhs(Function &F) {
-
-        for (BasicBlock &B : F) {
-
-          int size = bvec_mapping.size();
-          BitVector b(size, false);
-          lhs.insert({&B, b});
-
-          for (Instruction &I : B) {
-
-            if (I.isUsedInBasicBlock(&B) || I.isUsedOutsideOfBlock(&B)) {
-
-              unsigned ind = bvec_mapping[&I];
-              lhs[&B][ind] = 1;
-
-            }           
-          }
-        }    
-      }
-
-      void populate_rhs(Function &F) {
-
-        for (BasicBlock &B : F) {
-
-          int size = bvec_mapping.size();
-          BitVector b(size, false);
-          lhs.insert({&B, b});
-
-          for (Instruction &I : B) {
-
-            if (I.isUsedInBasicBlock(&B) || I.isUsedOutsideOfBlock(&B)) {
-
-              //do not process function calls
-              if (isa<CallInst>(&I))
-                continue;
-
-              // go over instruction args
-              for (User::op_iterator op = I.op_begin(), opE = I.op_end(); op != opE; ++op) {
-
-                Value* val = *op;
-                  
-                unsigned ind = bvec_mapping[val];
-                rhs[&B][ind] = 1;
-
-              }
-            }
-          }
-        }
-      }
-
-      void populate_use(Function &F){
-
-        for (BasicBlock &B : F) {
-
-          int size = bvec_mapping.size();
-          BitVector b(size, false);
-          lhs.insert({&B, b});
-
-          for (Instruction &I : B) {
-
-            if (!I.isUsedInBasicBlock(&B) && !I.isUsedOutsideOfBlock(&B) || isa<CallInst>(&I)) {
-
-              // go over instruction args
-              for (User::op_iterator op = I.op_begin(), opE = I.op_end(); op != opE; ++op) {
-
-                Value* val = *op;
-                  
-                unsigned ind = bvec_mapping[val];
-                use[&B][ind] = 1;
-
-              }
-            }
-          }
-        }
-
-      }
-
-      // sets
-      VMap bvec_mapping;
-      BBVal lhs;
-      BBVal rhs;
-      BBVal use;
-
-  };
-
   class dce : public FunctionPass {
 
   public:
     static char ID;
     dce() : FunctionPass(ID) { }
 
-    
+    void map_indexes(Function &F) {
+
+      unsigned ind = 0;
+
+      for (BasicBlock &B : F) {
+
+        for (Instruction &I : B) {
+
+          if (I.isUsedInBasicBlock(&B) || I.isUsedOutsideOfBlock(&B)) {
+
+            bvec_mapping.insert({&I, ind});
+            ind++;
+
+          }
+        }
+      }
+    }
+
+    void populate_lhs(Function &F) {
+
+      for (BasicBlock &B : F) {
+
+        int size = bvec_mapping.size();
+        BitVector b(size, false);
+        lhs.insert({&B, b});
+
+        for (Instruction &I : B) {
+
+          if (I.isUsedInBasicBlock(&B) || I.isUsedOutsideOfBlock(&B)) {
+
+            unsigned ind = bvec_mapping[&I];
+            lhs[&B][ind] = 1;
+
+          }           
+        }
+      }    
+    }
+
+    void populate_rhs(Function &F) {
+
+      for (BasicBlock &B : F) {
+
+        int size = bvec_mapping.size();
+        BitVector b(size, false);
+        lhs.insert({&B, b});
+
+        for (Instruction &I : B) {
+
+          if (I.isUsedInBasicBlock(&B) || I.isUsedOutsideOfBlock(&B)) {
+
+            //do not process function calls
+            if (isa<CallInst>(&I))
+              continue;
+
+            // go over instruction args
+            for (User::op_iterator op = I.op_begin(), opE = I.op_end(); op != opE; ++op) {
+
+              Value* val = *op;
+                
+              unsigned ind = bvec_mapping[val];
+              rhs[&B][ind] = 1;
+
+            }
+          }
+        }
+      }
+    }
+
+    void populate_use(Function &F){
+
+      for (BasicBlock &B : F) {
+
+        int size = bvec_mapping.size();
+        BitVector b(size, false);
+        lhs.insert({&B, b});
+
+        for (Instruction &I : B) {
+
+          if (!I.isUsedInBasicBlock(&B) && !I.isUsedOutsideOfBlock(&B) || isa<CallInst>(&I)) {
+
+            // go over instruction args
+            for (User::op_iterator op = I.op_begin(), opE = I.op_end(); op != opE; ++op) {
+
+              Value* val = *op;
+                
+              unsigned ind = bvec_mapping[val];
+              use[&B][ind] = 1;
+
+            }
+          }
+        }
+      }
+
+    }
+
 
     virtual bool runOnFunction(Function& F) {
+
+      map_indexes(F);
+      populate_lhs(F);
+      populate_rhs(F);
+      populate_use(F);
+
+      // initialize top element and bottom element according to the meetOp
+      unsigned size_bitvec = bvec_mapping.size();
+
+      //initialize data flow framework
+      DFF dff(&F, false, INTERSECTION, size_bitvec, &transfer_function, false);
+
+      // compute use and def sets here
+      populate_gen_kill(F);
+
 
       return false;
     }
@@ -145,6 +139,28 @@ namespace {
       AU.setPreservesAll();
     }
 
+    void populate_gen_kill(Function &F) {
+      int size = bvec_mapping.size();
+
+      for (BasicBlock &B : F) {
+
+        BitVector bvec(size, false);
+
+       
+
+        unsigned ind = bvec_mapping[&B];
+
+
+      }
+
+    }
+  private:
+
+    // sets
+    VMap bvec_mapping;
+    BBVal lhs;
+    BBVal rhs;
+    BBVal use;
 
   };
 
