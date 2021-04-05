0268cb9d819c99f609021c184b09bb4463cd91c4
saim <saim29@gmail.com>
2021-04-05 00:13:22 -0400

    added DCE micro benchmarks

---
+++ tests/test_dce1.c
@@ -0,0 +1,19 @@
+/*
+    ECE5984 Assignment 3
+    DCE micro benchmark 1
+
+*/
+int main() {
+    int a, b, c, d, e;
+
+    a = 100;
+    b = 5;
+    c = 25;
+
+    d = a + c * 4;
+    b+=b;
+    a +=a;
+    e = a/c;
+
+    return e;
+}
---
+++ tests/test_dce2.c
@@ -0,0 +1,25 @@
+/*
+    ECE5984 Assignment 3
+    DCE micro benchmark 2
+
+*/
+int main(){
+    int r1, r2, r3;
+
+    r1 = 1;
+    r2 = 2;
+    r3 = 3;
+
+    if(r2){
+        r1 = 5;
+    }
+    else{
+        r1 = 6;
+    }
+
+    r1 = r2 + 1;
+    r2 = r2 + 2;
+    r3 = r2 + r3;
+
+    return r3;
+}
